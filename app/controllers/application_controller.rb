d085aaf4475fa8621725f024641d5cd3c72b5daf
Rails <rails@mail.com>
2018-01-12 11:17:50 +0530

    Implemented graphql

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Clearance::Controller
   protect_from_forgery with: :exception
+
+  def authenticate_by_api_token!
+  	current_user
+  end
 end
