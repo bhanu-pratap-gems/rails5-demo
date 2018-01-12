4f8f7ba1bdd0efc8f7d88d751e8f9580c74a9c88
Rails <rails@mail.com>
2018-01-12 12:52:21 +0530

    Added posts and comments

--- app/graph/types/query_type.rb
+++ app/graph/types/query_type.rb
@@ -2,11 +2,18 @@ QueryType = GraphQL::ObjectType.define do
   name "Query"
   description "The query root of this schema"
 
-  field :viewer do
+  field :me do
     type UserType
     description "Current user"
     resolve ->(obj, args, ctx) {
       ctx[:current_user]
     }
+  end
+
+  field :post do
+    type PostType
+    argument :id, !types.ID
+    description "Find a Post by ID"
+    resolve ->(obj, args, ctx) { Post.find_by_id(args["id"]) }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   include Clearance::User
   has_secure_token :api_token
+
+  has_many :posts
 end
